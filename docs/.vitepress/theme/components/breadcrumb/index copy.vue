405d171b3856cd809cb4e0adadcd861b89dc4383
XXXG <279637548@qq.com>
2024-07-16 15:12:12 +0900

    删除dist文件

--- docs/.vitepress/theme/components/breadcrumb/index copy.vue
+++ docs/.vitepress/theme/components/breadcrumb/index copy.vue
@@ -1,9 +1,7 @@
 <template>
   <el-breadcrumb :separator-icon="ArrowRight" style="font-size:1.5rem">
-    <el-breadcrumb-item >
-      <a href="/">
-        <g-symbol  align-text family="rounded" fill weight="450" grade="0" size="200">home</g-symbol>
-      </a>
+    <el-breadcrumb-item :to="{ path: '/' }">
+      <g-symbol  align-text family="rounded" fill weight="450" grade="0" size="200">home</g-symbol>
     </el-breadcrumb-item>
     <el-breadcrumb-item v-for="(crumb, index) in breadcrumbs" :key="index" :to="crumb.path">
     <router-link :to="crumb.path">{{ crumb.label }}</router-link>
